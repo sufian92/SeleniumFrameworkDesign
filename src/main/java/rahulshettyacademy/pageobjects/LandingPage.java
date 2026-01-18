ea5788eab73e772c409c8cc7f08af122eb0abce8
sufianamara <sufianamara@outlook.com>
2026-01-18 16:36:50 +0100

    force fail

--- src/main/java/rahulshettyacademy/pageobjects/LandingPage.java
+++ src/main/java/rahulshettyacademy/pageobjects/LandingPage.java
@@ -24,7 +24,7 @@ public class LandingPage extends AbstractComponent{
 	//WebElement userEmails = driver.findElement(By.id("userEmail"));
 	//PageFactory
 	
-	@FindBy(id="userEmail")
+	@FindBy(id="userEmal")
 	WebElement userEmail;
 	
 	@FindBy(id="userPassword")
